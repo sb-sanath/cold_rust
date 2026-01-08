e7e794240d8db10f763652912db64ecda96d8dc0
Sanath SB <sanathsbadari@gmail.com>
2026-01-09 00:06:56 +0530

    Improvements to temperature conversion

--- temp_conversion/src/main.rs
+++ temp_conversion/src/main.rs
@@ -1,33 +1,36 @@
 use std::io;
 
 fn convert_fahrenheit_to_celcius(fahrenheit: f32) -> f32 {
-    return ((fahrenheit - 32 as f32) * 5 as f32) / 9 as f32
+    ((fahrenheit - 32.0) * 5.0) / 9.0
 }
 
 fn convert_celcius_to_fahrenheit(celcius: f32) ->f32 {
-    return (celcius * 1.8) + 32 as f32
+    (celcius * 1.8) + 32.0
+}
+
+fn read_user_input() -> f32 {
+    let mut user_input  = String::new();
+
+    io::stdin().read_line(&mut user_input).expect("Error reading input");
+
+    user_input.trim().parse().expect("Error parsing number")
+
 }
 
 fn main() {
-    let mut fahrenheit = String::new();
 
     println!("Please enter the temperature in Farenheit");
-    
-    io::stdin().read_line(&mut fahrenheit).expect("Error reading input");
-    let fahrenheit: f32 = fahrenheit.trim().parse().expect("Error parsing number");
 
+    let fahrenheit: f32 = read_user_input();
     let celcius: f32 = convert_fahrenheit_to_celcius(fahrenheit);
+
     println!("{} farenheit in celcius is : {}", fahrenheit, celcius);
 
     // 
 
-    let mut celcius = String::new();
-
     println!("Please enter the temperature in Celcius");
 
-    io::stdin().read_line(&mut celcius).expect("Error reading input");
-    let celcius: f32 = celcius.trim().parse().expect("Error parsing number");
-
+    let celcius: f32 = read_user_input();
     let fahrenheit: f32 = convert_celcius_to_fahrenheit(celcius);
     println!("{} celcius in fahrenheit is : {}", celcius, fahrenheit);
 
